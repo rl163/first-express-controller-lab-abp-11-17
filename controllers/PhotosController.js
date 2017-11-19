f88d14033ff42645c0cf4c8b59fe11574bc99280
Ryan Lee <ryanlee91@gmail.com>
2017-11-19 18:59:05 +0000

    automatically backed up by learn

--- controllers/PhotosController.js
+++ controllers/PhotosController.js
@@ -1,1 +1,19 @@
-const PhotoControlller
+const PhotosControlller
+
+PhotosController.Index = function(req,resp){
+
+}
+
+PhotosController.Show = function(req,resp){
+
+}
+
+PhotosController.Index = function(req,resp){
+
+}
+
+PhotosController.Index = function(req,resp){
+
+}
+
+module.exports = PhotosController
