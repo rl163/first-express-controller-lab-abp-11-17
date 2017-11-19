9d7c0a6b2979f395f94650cb95f2d8edb2548bd9
Ryan Lee <ryanlee91@gmail.com>
2017-11-19 19:01:15 +0000

    automatically backed up by learn

--- controllers/PhotosController.js
+++ controllers/PhotosController.js
@@ -1,19 +1,19 @@
 const PhotosControlller
 
 PhotosController.Index = function(req,resp){
-
+  resp.render("photos/index")
 }
 
 PhotosController.Show = function(req,resp){
-
+  resp.render("photos/show")
 }
 
 PhotosController.New = function(req,resp){
-
+  resp.render("photos/new")
 }
 
 PhotosController.Create = function(req,resp){
-
+  resp.render("photos/create")
 }
 
 module.exports = PhotosController
