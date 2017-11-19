2fb059de06e65d6478dc5e1b51e3bfdb4c86ee6f
Ryan Lee <ryanlee91@gmail.com>
2017-11-19 18:56:53 +0000

    automatically backed up by learn

--- controllers/SiteController.js
+++ controllers/SiteController.js
@@ -1,4 +1,12 @@
 const SiteController = {};
 
+SiteController.Index = function(req, resp){
+  
+}// index action 
+SiteController.About = function(req,resp){
+  
+}
+
+SiteController.Contact = function(req,resp){}
 
 module.exports = SiteController;
