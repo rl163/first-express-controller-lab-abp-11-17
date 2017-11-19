7b2b4514ab1b27fc8e58f51b41609969298a53fa
Ryan Lee <ryanlee91@gmail.com>
2017-11-19 19:01:52 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ var app = express()
 
 // Load Controllers
 const SiteController = require("./controllers/SiteController.js")
-
+const PhotosController = require("./controllers/PhotosController.js")
 // Routes
 app("/", SiteController.Index)
 app("/about", SiteController.About)
 app("/contact", SiteController.Contact)
+app("/photos", PhotosController.Index)
+app("/photos/:id", PhotosController.Show)
+app("/photos/new", PhotosController.New)
+app("/photos/create". PhotosController.Create)
+
 
 module.exports = app
